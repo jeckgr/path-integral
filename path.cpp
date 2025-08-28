07061856f04e30cd5bb4fadae347efd5ac0b756a
jeckgr <jenia.egorenkov@yandex.ru>
2025-08-29 01:56:08 +0300

    Add files via upload

---
+++ path.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+int main() {
+    int a = -10;
+    int b = 10;
+    int nt = 4;
+    long int ntraj = 500000000;
+    double m2 = 1.0;
+    double lambda = 0.0;
+    std::mt19937 rng(rand());
+    std::uniform_real_distribution<> uni(a,b);
+    double x[nt];
+    double c[nt];
+    for (int i = 0; i < nt; ++i) {
+        c[i] = 0;
+    }
+    double z = 0;
+    for (long int i = 0; i < ntraj; ++i) {
+        for (int j = 0; j < nt; ++j) {
+            x[j] = uni(rng);
+        }
+        double S = 0;
+        for (int j = 0; j < nt; ++j) {
+            S += pow(x[(j+1)%nt]-x[j],2)/2+m2*x[j]*x[j]/2+lambda*x[j]*x[j]*x[j]*x[j]/24;
+        }
+        for (int j = 0; j < nt; ++j) {
+            c[j] += pow((b-a),nt)*x[j]*x[0]*exp(-S)/ntraj;
+        }
+        z += pow((b-a),nt)*exp(-S)/ntraj;
+    }
+    for (int i = 0; i < nt; ++i) {
+        std::cout << acosh((c[(i+1)%nt]/z+c[(i-1+nt)%nt]/z)/(2*c[i]/z)) << '\n';
+    }
+}
