bf8a53cd4fab32cb001592213086296d4ab595a6
jeckgr <jenia.egorenkov@yandex.ru>
2025-08-30 00:43:37 +0300

    Update path_integral.cpp

--- path_integral.cpp
+++ path_integral.cpp
@@ -4,9 +4,10 @@
 int main() {
     int a = -6;
     int b = 6;
-    int nt = 6;
-    long int npath = 500000000;
-    double m2 = 0.25;
+    int nt = 4;
+    int npath = 1000000000;
+    double dt = 0.25;
+    double m2 = 1.0;
     double lambda = 0.0;
     std::mt19937 rng(rand());
     std::uniform_real_distribution<> uni(a,b);
@@ -16,20 +17,20 @@ int main() {
         c[i] = 0;
     }
     double z = 0;
-    for (long int i = 0; i < npath; ++i) {
+    for (int i = 0; i < npath; ++i) {
         for (int j = 0; j < nt; ++j) {
             x[j] = uni(rng);
         }
         double S = 0;
         for (int j = 0; j < nt; ++j) {
-            S += pow(x[(j+1)%nt]-x[j],2)/2+m2*x[j]*x[j]/2+lambda*x[j]*x[j]*x[j]*x[j]/24;
+            S += pow((x[(j+1)%nt]-x[j])/dt,2)/2+m2*x[j]*x[j]/2+lambda*x[j]*x[j]*x[j]*x[j]/24;
         }
         for (int j = 0; j < nt; ++j) {
-            c[j] += pow((b-a),nt)*x[j]*x[0]*exp(-S)/npath;
+            c[j] += pow((b-a),nt)*x[j]*x[0]*exp(-dt*S)/npath;
         }
-        z += pow((b-a),nt)*exp(-S)/npath;
+        z += pow((b-a),nt)*exp(-dt*S)/npath;
     }
     for (int i = 1; i < nt; ++i) {
-        std::cout << acosh((c[(i+1)%nt]/z+c[(i-1+nt)%nt]/z)/(2*c[i]/z)) << '\n';
+        std::cout << acosh((c[(i+1)%nt]/z+c[(i-1+nt)%nt]/z)/(2*c[i]/z))/dt << '\n';
     }
 }
